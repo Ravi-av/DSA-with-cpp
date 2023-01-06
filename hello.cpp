adc2c889123b905444867e576886a4678c738fa2
ravi-av <ravisrivastav2609@gmail.com>
2023-01-06 20:53:26 +0530

    all love babbar cpp codes

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a = 123;
+     cout<< a <<endl;
+
+     char b = 'v';
+     cout<<b<<endl;
+
+     bool bl= true;
+    cout<<bl<<endl;
+     double d1=1.78;
+     cout<<d1<<endl;
+    
+     int size = sizeof (a);
+     cout<<"size of a is "<<size<<endl;
+}
---
+++ operator.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // float a =2.0/5;
+    // cout<<a<<endl;
+    // int a=24,b=34;
+    // bool first = (a==b);
+    // cout<<first<<endl;
+
+    // bool second = (a>b);
+    // cout<<second<<endl;
+    
+    // bool third = (a<b);
+    // cout<<third<<endl;
+
+     
+    // bool fourth = (a>=b);
+    // cout<< fourth<<endl;
+
+     
+    // bool fifth = (a<=b);
+    // cout<<fifth<<endl;
+
+     
+    // bool sixth = (a!= b);
+    // cout<<sixth<<endl;
+int a = 0;
+cout<< !a<<endl;
+
+
+    
+    
+}
