ee14898dea74e8d2752f8a83097e5c7d771cfb06
ravi-av <ravisrivastav2609@gmail.com>
2023-07-28 21:30:20 +0530

    c++ codes

--- function.cpp
+++ function.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int power(int a, int b){
+    int ans = 1;
+    for(int i = 1; i<=b; i++){
+        ans = ans*a;
+}
+return ans; 
+}
+
 int main()
 {
-    // pow (a,b)
+int a,b;
+cin>>a>>b;
 
-    int a, b;
-    cin >> a >> b;
+int answer = power(a,b);
+cout<<"answer is "<<answer<<endl;
 
-    int ans = 1;
+return 0;
 
-    for (int i = 1;i <= b; i++)
-    {
-        ans = ans * a;
-    }
-    cout<<" answer is "<<ans<<endl;
-    int c, d;
-    cin >> c >> d;
-
-    int ans = 1;
-
-    for (int i = 1;i <= d; i++)
-    {
-        ans = ans * a;
-    }
-    cout<<" answer is "<<ans<<endl;
-    return 0;
 }
